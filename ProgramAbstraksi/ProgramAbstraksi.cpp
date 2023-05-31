0a3d354b33b149afe2c0375ab0fe18cb459d0aba
irajesicajanuaris <jjiraaa1@gmail.com>
2023-05-31 16:46:02 +0700

    menambahkan kembali fungso tampilan ke class AbstraksiKlas

--- ProgramAbstraksi/ProgramAbstraksi.cpp
+++ ProgramAbstraksi/ProgramAbstraksi.cpp
@@ -2,4 +2,18 @@
 using namespace std;
 
 class AbstraksiKlas {
+private: string x, y;
+public:
+
+	// Method untuk mengisi nilai
+	// Private member
+	void setXY(string a, string b) {
+		x = a;
+		x = b;
+	}
+	// Menampilkan nilai
+	void display() {
+		cout << "x = " << x << endl;
+		cout << "y = " << y << endl;
+	}
 };
